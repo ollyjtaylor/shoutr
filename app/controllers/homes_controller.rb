115807001746154687a6c6ba013084d41f854b1b
Olly Taylor <ollyjtaylor@live.co.uk>
2020-01-27 15:01:46 +0000

    added dashboard redirect

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,4 +2,13 @@ class HomesController < ApplicationController
   def show
     render
   end
+
+
+  private
+
+  def redirect_to_dashboard_if_signed_in
+    if signed_in?
+      redirect_to dashboard_path
+    end
+  end
 end
